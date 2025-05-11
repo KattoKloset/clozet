a38314f62c2b65144a208600db1c7f47daa16805
shagrirse <47131877+shagrirse@users.noreply.github.com>
2025-05-11 22:03:26 +0800

    chore: add husky and changed playwright tests

--- app/test/playwright.test.ts
+++ app/test/playwright.test.ts
@@ -1,25 +1,24 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test.setTimeout(35e3)
 
 test('send message', async ({ browser, page }) => {
   const viewer = await browser.newPage()
-  await viewer.goto('/')
 
-  await page.goto('/api/auth/signin')
-  await page.type('[name="name"]', 'test')
-  await page.click('[type="submit"]')
-
-  const nonce =
-    Math.random()
-      .toString(36)
-      .replace(/[^a-z]+/g, '')
-      .slice(0, 6) || 'nonce'
-  // await page.click('[type=submit]');
-  await page.type('[name=text]', nonce)
-  await page.click('[type=submit]')
-
-  await viewer.waitForSelector(`text=${nonce}`)
+  await page.goto('http://localhost:3000/')
+  await page.getByRole('button', { name: 'sign in', exact: true }).click()
+  await page.getByRole('textbox', { name: 'name' }).click()
+  await page.getByRole('textbox', { name: 'name' }).fill('test')
+  await page.getByRole('button', { name: 'Sign in with Mocked GitHub' }).click()
+  await page
+    .locator('div')
+    .filter({ hasText: /^Submit$/ })
+    .nth(1)
+    .click()
+  await page.locator('#text').click()
+  await page.locator('#text').fill('1qwer$#@!')
+  await page.getByRole('button', { name: 'Submit' }).click()
+  await expect(page.locator('[id="__next"]')).toContainText('1qwer$#@!')
   viewer.close()
 })
 
