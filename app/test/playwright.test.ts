059641e078b14d22b899452d9be9823afc514280
shagrirse <47131877+shagrirse@users.noreply.github.com>
2025-05-11 23:46:16 +0800

    fix: tests

--- app/test/playwright.test.ts
+++ app/test/playwright.test.ts
@@ -1,25 +1,19 @@
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
 
-test.setTimeout(35e3)
-
-test('send message', async ({ browser, page }) => {
-  const viewer = await browser.newPage()
-
-  await page.goto('http://localhost:3000/')
-  await page.getByRole('button', { name: 'sign in', exact: true }).click()
-  await page.getByRole('textbox', { name: 'name' }).click({ force: true })
+test('test submission', async ({ page }) => {
+  await page.goto('http://localhost:3000/api/auth/signin')
   await page.getByRole('textbox', { name: 'name' }).fill('test')
-  await page.getByRole('button', { name: 'Sign in with Mocked GitHub' }).click()
   await page
-    .locator('div')
-    .filter({ hasText: /^Submit$/ })
-    .nth(1)
-    .click()
-  await page.locator('#text').click()
-  await page.locator('#text').fill('1qwer$#@!')
-  await page.getByRole('button', { name: 'Submit' }).click()
-  await expect(page.locator('[id="__next"]')).toContainText('1qwer$#@!')
-  viewer.close()
-})
+    .getByRole('button', { name: 'Sign in with Mocked GitHub' })
+    .click({ force: true })
 
-export {}
+  const nonce =
+    Math.random()
+      .toString(36)
+      .replace(/[^a-z]+/g, '')
+      .slice(0, 6) || 'nonce'
+
+  await page.locator('#text').fill(nonce)
+  await page.getByRole('button', { name: 'Submit' }).click({ force: true })
+  await page.waitForSelector(`text=${nonce}`)
+})
